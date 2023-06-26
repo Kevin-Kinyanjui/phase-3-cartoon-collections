ea212ebaf22eb59acb454969bf940eb6e6e9be54
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-06-26 12:54:16 +0300

    complete assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| "#{call.capitalize}!" }
+end
+puts summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
+
+def long_planeteer_calls calls
+  calls.any? { |call| call.length > 4 }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese food_array
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food_array.find { |item| cheese_types.include?(item) }
 end
+
